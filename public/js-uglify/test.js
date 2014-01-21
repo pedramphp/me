124457d78fbcb710f9d4b2bd4e572b968305e07b
mahdi pedramrazi <pedramphp@gmail.com>
2014-01-21 00:35:43 -0800

    fixing facebook status

--- public/js-uglify/test.js
+++
@@ -1,7 +0,0 @@
-/*!
- * @author Osiris
- * @version 1.0.0
- * Copyright 2014.
-2014-01-20 
- */
-"use strict";var t=2,tyy=2;
--- public/js-uglify/test2.js
+++
@@ -1,7 +0,0 @@
-/*!
- * @author Osiris
- * @version 1.0.0
- * Copyright 2014.
-2014-01-20 
- */
-"use strict";var ta=2;
